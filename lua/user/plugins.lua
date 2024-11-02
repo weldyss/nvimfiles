4efb15437b36b936b772378e425ed74c3ab268b9
Weldys Santos <weldys@icloud.com>
2024-11-02 15:16:35 -0300

    wip on lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -79,7 +79,46 @@ return packer.startup(function(use)
   }
 
   -- LSP installations
-  use { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig" }
+  use { 
+    {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+        require("mason").setup({
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗"
+            }
+          }
+        })
+      end,
+    }, {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      opts = {
+        auto_install = true,
+      },
+    }, {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+        local lspconfig = require("lspconfig")
+        lspconfig.ruby_lsp.setup({
+          capabilities = capabilities
+        })
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+      end,
+    } }
 
   use('MunifTanjim/prettier.nvim')
   use({ "mhanberg/elixir.nvim", requires = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" }})
